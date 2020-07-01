6876962ae248a608ee6028921f10b77c052b56ac
Dan Weikart <danmweikart@gmail.com>
2020-07-01 07:28:51 -0700

    removed commented out code

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,39 +3,38 @@ const db = require("../models");
 
 router.post("/api/workouts", (req, res) => {
   console.log(req.body);
-  db.Workout.create({
-    // req.body
-  }).catch(err => {
-  db.Workout.create(req.body).catch(err => {
-    res.json(err);
+  db.Workout.create({}).catch(err => {
+    db.Workout.create(req.body).catch(err => {
+      res.json(err);
+    });
   });
-});
-router.put("/api/workouts/:id", (req, res) => {
-  console.log(req.body);
-  db.Workout.findByIdAndUpdate(req.params.id, {
-    $push: {
-      exercises: req.body,
-    },
-  })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
+  router.put("/api/workouts/:id", (req, res) => {
+    console.log(req.body);
+    db.Workout.findByIdAndUpdate(req.params.id, {
+      $push: {
+        exercises: req.body,
+      },
     })
-    .catch(err => {
-      res.json(err);
-    });
-});
-router.get("/api/workouts", (req, res) => {
-  db.Workout.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
 
-router.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({}).limit(7)
+  router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .limit(7)
       .then(dbWorkout => {
         res.json(dbWorkout);
       })
