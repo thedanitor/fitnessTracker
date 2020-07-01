43f78a416711b95820e11aeb8e7d0072497380b9
Dan Weikart <danmweikart@gmail.com>
2020-06-30 22:05:15 -0700

    Changed back to working code.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,11 +3,13 @@ const db = require("../models");
 
 router.post("/api/workouts", (req, res) => {
   console.log(req.body);
+  db.Workout.create({
+    // req.body
+  }).catch(err => {
   db.Workout.create(req.body).catch(err => {
     res.json(err);
   });
 });
-
 router.put("/api/workouts/:id", (req, res) => {
   console.log(req.body);
   db.Workout.findByIdAndUpdate(req.params.id, {
@@ -22,7 +24,6 @@ router.put("/api/workouts/:id", (req, res) => {
       res.json(err);
     });
 });
-
 router.get("/api/workouts", (req, res) => {
   db.Workout.find({})
     .then(dbWorkout => {
@@ -34,6 +35,14 @@ router.get("/api/workouts", (req, res) => {
 });
 
 router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).limit(7)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
   db.Workout.find({})
     .limit(7)
     .then(dbWorkout => {
