597752eedbdb709b18c3af7e840247d8691f0497
Dan Weikart <danmweikart@gmail.com>
2020-07-01 15:34:31 -0700

    cleaned up code

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,73 +2,52 @@ const router = require("express").Router();
 const db = require("../models");
 const { Workout } = require("../models");
 
-// router.post("/api/workouts", (req, res) => {
-//   console.log(req.body);
-//   db.Workout.create(
-//     // {}
-//     //  req.body
-//   ).catch(err => {
-//     // db.Workout.create(req.body).catch(err => {
-//       res.json(err);
-//     });
-//   });
-
-  router.post("/api/workouts", ({ body }, res) => {
-    const workout = new Workout(body);
-    console.log(workout);
-    db.Workout.create(workout)
+router.post("/api/workouts", ({ body }, res) => {
+  const workout = new Workout(body);
+  console.log(workout);
+  db.Workout.create(workout)
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
     .catch(err => {
       res.json(err);
     });
-    });
+});
 
-  router.put("/api/workouts/:id", (req, res) => {
-    console.log(req.body);
-    db.Workout.findByIdAndUpdate(req.params.id, {
-      $push: {
-        exercises: req.body,
-      },
+router.put("/api/workouts/:id", (req, res) => {
+  console.log(req.body);
+  db.Workout.findByIdAndUpdate(req.params.id, {
+    $push: {
+      exercises: req.body,
+    },
+  })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-  router.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-  router.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({})
-      .limit(7)
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-
-  // db.Workout.find({})
-  //   .limit(7)
-  //   .then(dbWorkout => {
-  //     res.json(dbWorkout);
-  //   })
-  //   .catch(err => {
-  //     res.json(err);
-  //   });
-// });
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .limit(7)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
